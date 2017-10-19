91c1ab26fb7fa20d3d3927cb64af223b615fbf35
Shaun Harker <sharker81@gmail.com>
2017-10-18 22:04:26 -0400

    python binding

--- install.sh
+++ install.sh
@@ -1,31 +1,5 @@
-#!/bin/bash
-# install.sh [--prefix=PREFIX] [--build=BUILDTYPE]    \
-#            [--search=SEARCHPATH] [CMake arguments]
-#  
-#  Build the project with the supplied configurations,
-#    or else default values.
-#
-#   PREFIX gives the location to install.
-#   BUILDTYPE is either Debug or Release 
-#     (or some other CMake recognizable build type)
-#   SEARCHPATH is an optional location to search for headers 
-#     and libraries (i.e. SEARCHPATH/include and SEARCHPATH/lib)
-#   The default setting for PREFIX is /usr/local unless it is not writable
-#     in which case it is ~/.local.
-#   The default setting for BUILDTYPE is Release
-#   The default setting for SEARCHPATH is to be equal to PREFIX
-#   Additional arguments will be passed to CMake. Any paths in these arguments
-#   should be absolute.
+# installer script
 
-SRC_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-build="$SRC_ROOT/.install/build.sh"
+git submodule update --init --recursive
+pip install . --ignore-installed --no-cache-dir
 
-# Parse command line arguments
-source $SRC_ROOT/.install/parse.sh
-
-$SRC_ROOT/.install/phat.sh --prefix=$SRC_ROOT/src/phat/ || exit 1
-MASS="$MASS -DCMAKE_PHAT_PATH=$SRC_ROOT/src/phat"
-
-# Build "protein2diagram"
-cd ${SRC_ROOT}
-$build --prefix=$PREFIX --searchpath=$SEARCHPATH --build=$BUILDTYPE $MASS || exit 1
