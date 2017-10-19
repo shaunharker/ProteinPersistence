4ef02ff90e99c6e976af146166be333b5c8c8a42
Shaun Harker <sharker81@gmail.com>
2017-10-19 14:01:05 -0400

    introduced parallel script

---
+++ scripts/Compute_PDB_Diagrams.py
@@ -0,0 +1,44 @@
+# Set up environment
+from Bio.PDB import *
+import re
+import ProteinPersistence
+import json
+import multiprocessing as mp
+import sys
+
+def Compute_PDB_PersistenceDiagrams(filename):
+    # Load an example PDB file using BioPython
+    p = PDBParser()
+    structure = p.get_structure('',filename)
+    # Create a list of entries [x, y, z, r] representing (x,y,z) coordinates of 
+each atom
+    # and van der Waals radius of that atom
+    def atom_shortname(s):
+        return s[re.search("[A-Z]", s).start()]
+
+    def vanderWaalsRadius(atom_name):
+        """ 
+        Return van der Waals radius associated with 'atom_name'
+        """
+        return { "H" : 1.2, "C" : 1.7 , "N" : 1.55, "O" : 1.52, "S" : 1.8}[atom_
+name];
+    xyzr_list = [ list(atom.get_coord()) + [vanderWaalsRadius(atom_shortname(ato
+m.get_name()))] \
+                  for atom in structure.get_atoms() ]
+    # Compute 
+    return ProteinPersistence.pdb2persistence(xyzr_list)
+
+def PerformJobsWithMultiprocessing(manifest_filename, path, savepath):
+    with open(manifest_filename) as f:
+        manifest = f.readlines()
+    pool = mp.Pool(processes=20)
+    def ProcessFile(filename):
+        diagrams = Compute_PDB_PersistenceDiagrams( path + '/' + filename)
+        with open(savepath + '/' + filename + '_persistence.json', 'w') as outfile:
+            json.dump(diagrams, outfile)    
+    pool.map(ProcessFile, manifest)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: Compute_PDB_Diagrams.py <manifest_file> <path_of_pdb_files> <path_to_save_output>")
+    PerformJobsWithMultiprocessing(sys.argv[1], sys.argv[2], sys.argv[3])
